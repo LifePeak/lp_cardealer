7356433005e8b9b9b2bbd34b955939adaca6acca
Goldberg <d2041827@gmail.com>
2023-09-14 17:24:31 +0200

    test commit
    Only testing commit

--- client/main.lua
+++ client/main.lua
@@ -9,6 +9,7 @@ local Keys = {
     ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
     ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
+--print
 
 local m1 = false 
 local color = 0
